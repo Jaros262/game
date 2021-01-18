fcc626f6af101b620fafe47d04b691506350c14c
Jaros262 <matysekjaros@gmail.com>
2021-01-18 21:06:56 +0100

    alfa version

--- my.js
+++ my.js
@@ -1,85 +1,112 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
+const restart = document.getElementById('restart')
 canvas.width = 650;
 canvas.height = 650;
+let score = 0;
+let gameFrame = 0;
+ctx.font = '35px Arial';
 
-
-
-let player = {
-    sprite: new Image(),
-    place: new Image(),
+const movement = {
     x: 50,
     y: 50,
-    width: 50,
-    height: 50,
-    speed: 9,
-    moving: false,
-    init: function(){
-        this.sprite.src = "person.png";
-        this.place.src = "place.png";
-    },
-    draw: function(){
-        ctx.drawImage(this.sprite, this.x, this.y, this.width, this.height);
-        
-    },
-    paint: function(key){
-        if(key == " "){drawImage(this.place, this.x, this.y, this.width, this.height);}
-    },
+    click: false
+}
+window.addEventListener("keydown", function(event){
+    movement.click = true;
+    console.log(event);
+    player.move(event.key);
+});
+window.addEventListener("keyup", function(event){
+    movement.click = false;
+})
 
-    move: function(key){
+const playersprite = new Image();
+playersprite.src = "person.png";
+class Player {
+    constructor(){
+        this.x = canvas.width-600;
+        this.y = canvas.height-600;
+        this.width = 50;
+        this.height = 50;
+        this.speed = 9;
+        this.moving = false;
+        this.position = this.x;
+    }
+    draw(){
+        ctx.drawImage(playersprite, this.x, this.y, this.width, this.height);
+    }
+    move(key){
         console.log(key);
         if (key == "ArrowRight" && player.x < 550){this.x += 50;}
         if (key == "ArrowLeft" && player.x > 50){this.x -= 50;}
         if (key == "ArrowUp" && player.y > 50){this.y -= 50;}
         if (key == "ArrowDown" && player.y < 550){this.y += 50;}
-        
     }
-    /*action: function(space){
-        console.log(space);
-        if (space == " "){action.draw();}
-    }*/
-};
+}
+const player = new Player();
 
-/*let action = {
-    place: new Image(),
-    width: 50,
-    height: 50,
-    init: function(){this.place.src = "place.png";},
-    draw: function(key){
-        if(key == " "){this.place, player.x, player.y, player.
-            width, player.height;}
-        },
+//bubbles
+const bubblesArray = [];
+class Bubble {
+    constructor(){
+        this.x =canvas.width + 50;
+        this.y = Math.random() * canvas.height;
+        this.radius = 25;
+        this.speed = Math.random() * 2 + 1;
+        this.distance;
+        this.counted = false;
     }
-*/
-let game = {
-    background: new Image(),
-    init: function(){
-        this.background.src = "background.png";
-        player.init();
-    },
-    draw: function(){
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        ctx.drawImage(this.background, 0, 0, canvas.width, canvas.height);
-        player.draw();
+    update(){
+        this.x -= this.speed;
+        const dx = this.x - player.x;
+        const dy = this.y - player.y;
+        this.distance = Math.sqrt(dx*dx + dy*dy);
+    }
+    draw(){
+        ctx.fillStyle = 'black';
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.radius,0,Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+        ctx.stroke();
     }
 }
 
+function handleBubbles(){
+    if(gameFrame % 50 ==0){
+        bubblesArray.push(new Bubble());
+    }
+    for (let i = 0; i < bubblesArray.length; i++){
+        bubblesArray[i].update();
+        bubblesArray[i].draw();  
+    }
+    for (let i = 0; i< bubblesArray.length; i++){
+        if (bubblesArray[i].y < 0 - bubblesArray[i].radius * 2){
+            bubblesArray.splice(i, 1);
+        }
+        if (bubblesArray[i].distance < bubblesArray[i].radius + player.position){
+            if (!bubblesArray[i].counted){
+                score++;
+                bubblesArray[i].counted = true;
+                bubblesArray.splice(i, 1);
+            }
+        }
+    }
+}
+
+restart.addEventListener('click', resetCanvas);
+function resetCanvas(){
+    score = 0;
+}
 
 function animate(){
-    game.draw();
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    handleBubbles();
+    player.draw();
+    ctx.fillStyle = 'black';
+    ctx.fillText('score: ' + score, 60, 34);
+    gameFrame++;
     requestAnimationFrame(animate);
-    /*if (key ==  " "){action.draw;}*/
 }
-
-window.addEventListener("keydown", function(event){
-    console.log(event);
-    player.move(event.key);
-});
-
-window.addEventListener("keypress", function(e){
-    console.log(e);
-    game.draw(e.key)
-});
-
-game.init();
 animate();
